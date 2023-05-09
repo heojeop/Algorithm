8d0b34697a266eba2f971aa11cdd0d718108e1c0
heojeop <20191246@vision.hoseo.edu>
2023-05-09 16:03:33 +0900

    Regex

--- main.cpp
+++ main.cpp
@@ -1,92 +1,75 @@
+#include <regex>
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
 
 
-class PriorityQueue{
-	public:
-	PriorityQueue(){}
+void Pn() {
+	std::regex PN("^(?:\\+\\d{1,3})?[- ]?\\d{2,3}[- ]\\d{3,4}[- ]\\d{4}$");
+	std::string PhoneNumber;
 
-	void push(int value){
-		heap.push_back(value);
-		siftUp(heap.size() - 1);
-	}
-
-	int pop(){
-
-		if(heap.empty()){
-			throw std::runtime_error("Empty heap");
-		}
-		int minValue = heap[0];
-		heap[0] = heap.back();
-		heap.pop_back();
-		siftDown(0);
-
-		return minValue;
-	}
-
-	bool empty() const{
-		return heap.empty();
-	}
-	private:
-	std::vector<int> heap;
-
-
-	int getParent(int index){
-		return (index - 1) / 2;
-	}
-
-	int getLeftChild(int index){
-		return 2 * index + 1;
-	}
-
-	int getRightChild(int index){
-		return 2 * index + 2;
-	}
-
-	void siftUp(int index){
-		while (index > 0 && heap[index] < heap[getParent(index)])
-		{
-			std::swap(heap[index], heap[getParent(index)]);
-			index = getParent(index);
-		}
-		
-	}
-
-	void siftDown(int index){
-		int minIndex = index;
-		int leftChild = getLeftChild(index);
-		int rightChild = getRightChild(index);
-
-		if(leftChild < heap.size() && heap[leftChild] < heap[minIndex]){
-			minIndex = leftChild;
-		}
-		if(rightChild < heap.size() && heap[rightChild] < heap[minIndex]){
-			minIndex = rightChild;
-		}
-		if(index != minIndex){
-			std::swap(heap[index], heap[minIndex]);
-			siftDown(minIndex);
-		}
-	}
-};
-
-int main(void) {
-
-	PriorityQueue pq;
-
-	pq.push(5);
-	pq.push(2);
-	pq.push(6);
-	pq.push(7);
-	pq.push(1);
-
-	while (!pq.empty())
+	while (1)
 	{
-		std::cout << pq.pop() << " ";
+		printf("전화번호를 입력 해 주세요 = ");
+		std::getline(std::cin, PhoneNumber);
+
+		if (std::regex_match(PhoneNumber, PN)) {
+			printf("전화번호 형식이 올바릅니다.\n");
+			break;
+		}
+		else
+		{
+			printf("전화번호 형식이 올바르지 않습니다.\n");
+		}
 	}
+}
+
+void email() {
+
+	std::regex EM("^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$");
+	std::string Email;
+
+	while (1)
+	{
+		printf("이메일을 입력 해 주세요 = ");
+		std::getline(std::cin, Email);
+
+		if (std::regex_match(Email, EM)) {
+			printf("이메일 형식이 올바릅니다.\n");
+
+		}
+		else
+		{
+			printf("이메일 형식이 올바르지 않습니다.\n");
+		}
+	}
+}
+
+void Password() {
+	std::regex PW("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$");
+	std::string Password;
+
+	while (1)
+	{
+		printf("비밀번호를 입력 해 주세요 = ");
+		std::getline(std::cin, Password);
+
+		if (std::regex_match(Password, PW)) {
+			printf("비밀번호 형식이 올바릅니다.\n");
+
+		}
+		else
+		{
+			printf("비밀번호 형식이 올바르지 않습니다.\n");
+		}
+	}
+}
+int main(void) {
 	
-	return 0;
+
+	Password();
+
+
+
 }
 
 
